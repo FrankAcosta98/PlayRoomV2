abe1e1e46c66c7f1d2119d0467ad60edc4a93620
FrankAcosta <frank-nova98@live.com>
2020-03-31 21:18:45 -0600

    --- Auto-Git Commit ---

--- Assets/Scripts/MainChar.cs
+++ Assets/Scripts/MainChar.cs
@@ -8,7 +8,23 @@ public class MainChar : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float spd;
-    void FixedUpdate()
-
-
+    CharCrt Input;
+    Vector2 move;
+    void Awake()
+    {
+        Input = new CharCrt();
+        Input.Control.move.performed += ctx => move = ctx.ReadValue<Vector2>();
+    }
+    void Update()
+    {
+        rb.MovePosition(rb.position + move * spd * Time.deltaTime);
+    }
+    void OnEnable()
+    {
+        Input.Enable();
+    }
+    void OnDisable()
+    {
+        Input.Disable();
+    }
 }
