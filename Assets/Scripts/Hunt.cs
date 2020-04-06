079196959cbd2ed6ee30eafe0230fbc53519b05a
FrankAcosta <frank-nova98@live.com>
2020-04-06 17:53:58 -0500

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/Hunt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class Hunt : MonoBehaviour
+{
+    public AIDestinationSetter destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destination.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        destination.enabled = true;
+    }
+}
