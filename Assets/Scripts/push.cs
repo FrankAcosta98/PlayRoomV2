c3b7fa4163e81b555b97a31f7175adfd75b2b634
FrankAcosta <frank-nova98@live.com>
2020-05-28 05:33:15 -0500

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/push.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class push : MonoBehaviour
+{
+    private bool usable = false;
+    Transform Player;
+    public Rigidbody2D rb;
+    private bool move = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (move && Vector2.Distance(Player.position, gameObject.transform.position) > 2.53f)
+        {
+            rb.MovePosition(Vector2.MoveTowards(gameObject.transform.position, Player.position, Player.GetComponent<MainChar>().spd * Time.fixedDeltaTime));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            usable = true;
+            Player = other.gameObject.transform;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+            usable = false;
+    }
+}
---
+++ Assets/box.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class box : MonoBehaviour
+{
+    private bool usable = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name=="Player"&&Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) < 3f)
+            usable = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player" && Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) >= 3f)
+            usable = false;
+    }
+}
