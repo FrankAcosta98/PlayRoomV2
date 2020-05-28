c1e26267f1d878b151c8f40aa1044564d2e102a8
FrankAcosta <frank-nova98@live.com>
2020-05-28 06:03:20 -0500

    --- Auto-Git Commit ---

--- Assets/box.cs
+++ Assets/box.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class box : MonoBehaviour
 {
     private bool usable = false;
+    private bool holded = false;
+    GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,29 @@ public class box : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (usable && Input.GetKeyDown(KeyCode.E))
+        {
+            holded = true;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        }
+        if (holded && Input.GetKeyDown(KeyCode.E))
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if (holded)
+        {
+            gameObject.transform.position = Player.transform.position;
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.name=="Player"&&Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) < 3f)
+        if (other.name == "Player" && Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) < 3f)
+        {
+            Player = other.gameObject;
             usable = true;
+        }
     }
     void OnTriggerExit2D(Collider2D other)
     {
