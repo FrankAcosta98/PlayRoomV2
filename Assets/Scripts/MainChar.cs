2843f742dec3935506c1563ecd15fdfef73376cc
FrankAcosta <frank-nova98@live.com>
2020-07-15 03:21:43 -0500

    --- Auto-Git Commit ---

--- Assets/Scripts/MainChar.cs
+++ Assets/Scripts/MainChar.cs
@@ -89,8 +89,7 @@ public class MainChar : MonoBehaviour
 
         if ((Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0) && anim.speed == 1f)
         {
-            //anim.GetCurrentAnimatorClipInfo(0)[0].clip.name;
-            //anim.PlayInFixedTime(, 0,0f);
+            //anim.PlayInFixedTime(anim.GetCurrentAnimatorClipInfo(0)[0].clip.name, 0,0f);
             anim.speed = 0f;
             
         }
