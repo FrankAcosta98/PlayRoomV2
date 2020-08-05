cafc65f09ebad4e34dc1d7a200b7f5e837e10300
FrankAcosta <frank-nova98@live.com>
2020-08-05 02:32:49 -0500

    --- Auto-Git Commit ---

--- Assets/Scripts/push.cs
+++ Assets/Scripts/push.cs
@@ -4,51 +4,25 @@ using UnityEngine;
 
 public class push : MonoBehaviour
 {
-    private bool usable = false;
-    GameObject Player;
-    public Rigidbody2D rb;
-    private bool move = false;
-    private bool holded = false;
     // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-    void Update()
-    {
-        if (usable && Input.GetKeyDown(KeyCode.E))
-        {
-            usable = false;
-            move = true;
-            holded = true;
-
-        }
-        else if (holded && Input.GetKeyDown(KeyCode.E) && usable == false)
-
-        {
-            move = false;
-            holded = false;
-        }
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (move && Vector2.Distance(Player.transform.position, gameObject.transform.position) > 2.53f)
-        {
-            rb.MovePosition(Vector2.MoveTowards(gameObject.transform.position, Player.transform.position, Player.GetComponent<MainChar>().spd * Time.fixedDeltaTime));
-        }
-    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player" && Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) < 3f)
         {
-            Player = other.gameObject;
-            usable = true;
+            if (other.gameObject.GetComponent<LightControl>().Holded == true)
+            {
+                other.gameObject.GetComponent<LightControl>().Holded = false;
+                other.gameObject.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().pointLightOuterRadius = 2.8f;
+            }
+            other.gameObject.GetComponent<Animator>().SetBool("push", true);
         }
     }
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.name == "Player" && Vector2.Distance(other.gameObject.transform.position, gameObject.transform.position) >= 3f)
-            usable = false;
+        {
+            other.gameObject.GetComponent<Animator>().SetBool("push", false);
+        }
     }
 }
