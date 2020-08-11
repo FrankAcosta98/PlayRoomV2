d0c42a217f96a1c0cc937e81d5f1fb470e1864d6
FrankAcosta <frank-nova98@live.com>
2020-08-11 02:14:24 -0500

    --- Auto-Git Commit ---

---
+++ Assets/Scripts/AINonLethalNoPath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pathfinding
+{
+
+    [UniqueComponent(tag = "ai.destination")]
+
+    public class AINonLethal : VersionedMonoBehaviour
+    {
+        /// <summary>The object that the AI should move to</summary>
+
+        public GameObject player;
+        float switchTime = float.PositiveInfinity;
+        IAstarAI ai;
+        void Start()
+        {
+            
+        }
+        void OnEnable()
+        {
+            ai = GetComponent<IAstarAI>();
+            // Update the destination right before searching for a path as well.
+            // This is enough in theory, but this script will also update the destination every
+            // frame as the destination is used for debugging and may be used for other things by other
+            // scripts as well. So it makes sense that it is up to date every frame.
+            if (ai != null) ai.onSearchPath += Update;
+        }
+
+        void OnDisable()
+        {
+            if (ai != null) ai.onSearchPath -= Update;
+        }
+
+        /// <summary>Updates the AI's destination every frame</summary>
+        void Update()
+        {
+            //Debug.Log(chillLevel);
+            if ((ai != null) && (Vector2.Distance(player.transform.position, gameObject.transform.position) > 2.53f)) ai.destination = player.transform.position;
+            
+        }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.name == "Player")
+            {
+                Destroy(gameObject, 3f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/spinEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class spinEffect : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.SetColor("tone", Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
