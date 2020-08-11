9e941a14f73a1d93a72fd1fd3c8e1f8f56d64a9c
FrankAcosta <frank-nova98@live.com>
2020-08-11 17:20:13 -0500

    --- Auto-Git Commit ---

--- Assets/Scripts/spinEffect.cs
+++ Assets/Scripts/spinEffect.cs
@@ -11,27 +11,24 @@ public class spinEffect : MonoBehaviour
     public Color yellow;
     private Color def;
     private Color next;
-    private Color full;
+    public Color full;
     public float changeSpeed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Renderer>().material.SetColor("Color_7BE80810", red);
+        
+        GetComponent<Renderer>().material.SetColor("Color_7BE80810", red+full);
         GetComponent<Light2D>().color = red;
         def = red;
         next = yellow;
-        //full.a;5882353
-        full.a = 4117647;
-        full.b = 0;
-        full.g = 0;
-        full.r = 0;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(def.a);
+        Debug.Log(def.a+full.a);
         GetComponent<Renderer>().material.SetColor("Color_7BE80810", def+full);
         GetComponent<Light2D>().color = def;
         def = Vector4.MoveTowards(def, next, changeSpeed);
